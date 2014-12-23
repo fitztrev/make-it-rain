76d49f7b95c6cc45e96ba7d2e2c42f93a4cfcd5c
Luke Miles <luke@lukemil.es>
2014-12-22 21:56:40 -0800

    add support for checking Stripe's API to verifiy webhooks

--- config.sample.php
+++ config.sample.php
@@ -13,9 +13,23 @@ $slack = array(
     'webhook_url' => '', // leave blank to disable Slack
 );
 
+// How you want to verify incoming webhooks. 
+// Either via the secret string below Stripe's API, or both.
+// Options are either "secret", "API" ,or "both"
+
+$verification = "secret";
+
+// Your Stripe API info. Only needed if you set $verification to "API" or "both"
+// Setup instructions at https://github.com/fitztrev/make-it-rain
+
+$stripe = array(
+  "secret_key"      => "sk_z",
+);
+
 // Secret identifier known only to Stripe so nobody can ping your webhook
 // and give you a false notification. Change this to something random.
 // Be sure to update the webhook in your Stripe account
+// This is only checked if $verification is set to "secret" or "both"
 $secret = 'abc123';
 
 // Add your own custom gifs that will be randomly added to the rotation
